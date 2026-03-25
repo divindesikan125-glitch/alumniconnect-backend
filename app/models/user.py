c5e6032630321e013e4ddc2ee35a0f015ce119bc
divindesikan125-glitch <divindesikan125@gmail.com>
2026-03-26 00:43:55 +0530

    Update user.py

--- app/models/user.py
+++ app/models/user.py
@@ -1,7 +1,7 @@
 from sqlalchemy import Column, Integer, String, Boolean, DateTime, ForeignKey
 from sqlalchemy.orm import relationship
 from datetime import datetime
-from app.db.database import Base
+from .db.database import Base
 
 # In your User model
 @property
